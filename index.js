11986bbcfb32847a9017b87b86ad67345953fa1f
firozahmmed1 <firozahmmed05@gmail.com>
2023-06-11 08:14:30 +0600

    four-update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vxcd8cz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,9 +27,40 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const userCollection=client.db('artsCraft').collection('users');
+    app.post('/jwt', (req, res)=>{
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '1h' })
+      res.send(token)
+    })
     
+    app.post('/users', async(req, res)=>{
+        const body = req.body;
+        const query = {email : body.email};
+        const stayUser = await userCollection.findOne(query)
+        if(stayUser){
+            return 
+        }
+        const result = await userCollection.insertOne(body)
+        res.send(result)
+    })
     
-    
+    app.get('/users', async(req,res)=>{
+        const result = await userCollection.find().toArray();
+        res.send(result)
+    })
+
+    app.put('/users/:id', async(req,res)=>{
+        const id = req.params.id;
+        const filter = {_id : new ObjectId(id)}
+        const options = { upsert: true };
+        const UpdateDoc = {
+            $set:{
+                role:"instructor"
+            }
+        }
+        const result = await userCollection.updateOne(filter, UpdateDoc, options)
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
